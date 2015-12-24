368d34f454a7cda24ad1d343837cd52b38f49551
Mauro E. Henaro <muro.mehs.stroke.2672@gmail.com>
2015-12-23 18:01:39 -0600

    Se crea el proyecto de utilerias para mym

---
+++ MyMUtileriasGenericas/AccesoDatos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMUtileriasGenericas.AccesoDatos
+{
+    /// <summary>
+    /// Clase estática encargada de conectar a una BD
+    /// </summary>
+    /// <remarks>
+    /// Creado por: T.P. Mauro Etzael Henaro Sánchez
+    /// Version: 23.12.2015
+    /// </remarks>
+    public static class Conexion
+    {
+
+    }
+}
