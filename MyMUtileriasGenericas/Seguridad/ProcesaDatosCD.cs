816a408f1b08672a31c614252f500423c8c2e676
Mauro E. Henaro <muro.mehs.stroke.2672@gmail.com>
2015-12-30 18:46:12 -0600

    se termina de desarrollar, implementar y probar los métodos de cifrado con MD5 y DES

--- MyMUtileriasGenericas/Seguridad/ProcesaDatosCD.cs
+++ MyMUtileriasGenericas/Seguridad/ProcesaDatosCD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -21,6 +22,7 @@ namespace MyMUtileriasGenericas.Seguridad
         private const string PASSWORDHASH = "P@@Sw0rd";
         private const string SALTKEY = "S@LT_KEY";
         private const string VIKEY = "@1B2c3D4e5F6g7H8";
+        private static byte[] bytes = ASCIIEncoding.ASCII.GetBytes("megaloma");
 
         /// <summary>
         /// Método que digiere la cadena de caracteres
@@ -46,6 +48,130 @@ namespace MyMUtileriasGenericas.Seguridad
                         cadena.Append(hash[i].ToString("x2"));
                 }
                 resultado = cadena.ToString();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return resultado;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textoOriginal"></param>
+        /// <returns></returns>
+        public static string CifraTextoEnDES(string textoOriginal)
+        {
+            string resultado = string.Empty;
+            try
+            {
+                if (string.IsNullOrEmpty(textoOriginal))
+                    throw new ArgumentNullException("El texto acifrar no puede ser nulo o vacío");
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                MemoryStream memory = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memory, 
+                                                             des.CreateEncryptor(bytes, bytes), 
+                                                             CryptoStreamMode.Write);
+                StreamWriter writer = new StreamWriter(cryptoStream);
+                writer.Write(textoOriginal);
+                writer.Flush();
+                cryptoStream.FlushFinalBlock();
+                writer.Flush();
+                resultado = Convert.ToBase64String(memory.GetBuffer(), 0, (int)memory.Length);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return resultado;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textoOriginal"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string CifraTextoEnDES(string textoOriginal,string password)
+        {
+            string resultado = string.Empty;
+            try
+            {
+                if (string.IsNullOrEmpty(textoOriginal))
+                    throw new ArgumentNullException("El texto a cifrar no puede ser nulo o vacío");
+                if (string.IsNullOrEmpty(password))
+                    throw new ArgumentNullException("El password para cifrar no puede ser nulo o vacío");
+                bytes = ASCIIEncoding.ASCII.GetBytes(password);
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                MemoryStream memory = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memory,
+                                                             des.CreateEncryptor(bytes, bytes),
+                                                             CryptoStreamMode.Write);
+                StreamWriter writer = new StreamWriter(cryptoStream);
+                writer.Write(textoOriginal);
+                writer.Flush();
+                cryptoStream.FlushFinalBlock();
+                writer.Flush();
+                resultado = Convert.ToBase64String(memory.GetBuffer(), 0, (int)memory.Length);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return resultado;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textoCifrado"></param>
+        /// <returns></returns>
+        public static string DescifraTextoDES(string textoCifrado)
+        {
+            string resultado = string.Empty;
+            try
+            {
+                if (string.IsNullOrEmpty(textoCifrado))
+                    throw new ArgumentNullException("El texto a descifrar no puede ser nulo o vacío");
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                MemoryStream memory = new MemoryStream(Convert.FromBase64String(textoCifrado));
+                CryptoStream cryptoStream = new CryptoStream(memory,
+                                                             des.CreateDecryptor(bytes, bytes),
+                                                             CryptoStreamMode.Read);
+                StreamReader reader = new StreamReader(cryptoStream);
+                resultado = reader.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return resultado;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textoCifrado"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string DescifraTextoDES(string textoCifrado,string password)
+        {
+            string resultado = string.Empty;
+            try
+            {
+                if (string.IsNullOrEmpty(textoCifrado))
+                    throw new ArgumentNullException("El texto a descifrar no puede ser nulo o vacío");
+                if (string.IsNullOrEmpty(password))
+                    throw new ArgumentNullException("El password para descifrar no puede ser nulo o vacío");
+                bytes = ASCIIEncoding.ASCII.GetBytes(password);
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                MemoryStream memory = new MemoryStream(Convert.FromBase64String(textoCifrado));
+                CryptoStream cryptoStream = new CryptoStream(memory,
+                                                             des.CreateDecryptor(bytes, bytes),
+                                                             CryptoStreamMode.Read);
+                StreamReader reader = new StreamReader(cryptoStream);
+                resultado = reader.ReadToEnd();
             }
             catch (Exception e)
             {
--- MyMUtileriasGenericas/TestUtilerias.cs
+++ MyMUtileriasGenericas/TestUtilerias.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyMUtileriasGenericas.AccesoDatos;
+using MyMUtileriasGenericas.Seguridad;
 
 namespace MyMUtileriasGenericas
 {
@@ -14,13 +15,14 @@ namespace MyMUtileriasGenericas
     class TestUtilerias
     {
         private enum ListaAcciones { AYUDA=0,LISTA_ACCIONES,CIFRADO_MD5,CIFRADO_DES, DESCIFRADO_DES,CIFRADO_RFC289, DESCIFRADO_RFC289 }
+        private static string[] cadenas;
 
         private static void ConfiguraLogNet()
         {
 
         }
 
-        public static void CreaInstrucciones()
+        private static void CreaInstrucciones()
         {
             string pathArchivo = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), @"ArchivosPlanos\Instrucciones.txt");
             List<string> instrucciones=LecturaArchivos.LeeArchivoLineaPorLinea(pathArchivo);
@@ -28,11 +30,67 @@ namespace MyMUtileriasGenericas
             {
                 Console.WriteLine(renglon);
             }
+            Console.WriteLine("Lista de acciones principales del ejecutable:");
+            CreaListaAcciones();
         }
 
-        public static void CreaListaAcciones()
+        private static void CreaListaAcciones()
         {
+            var a = Enum.GetValues(typeof(ListaAcciones));
+            int cont = 1;
+            Console.WriteLine();
+            foreach (ListaAcciones item in a)
+            {
+                Console.WriteLine(cont+".- "+item.ToString().ToLower());
+                cont++;
+            }
+        }
 
+        private static void ProcesaCadenaParaMD5(string[] aProcesar)
+        {
+            foreach (string item in aProcesar)
+            {
+                cadenas = item.Split(';');
+                Console.WriteLine();
+                Console.WriteLine(string.Format("texto a cifrar: {0}", cadenas[0]));
+                Console.WriteLine();
+                Console.WriteLine("Resultado en UpperCase:");
+                Console.WriteLine();
+                string cifrado = ProcesaDatosCD.ObtienePassword(cadenas[1], true);
+                Console.WriteLine(cifrado);
+                Console.WriteLine();
+                Console.WriteLine("Resultado en LowerCase:");
+                Console.WriteLine();
+                cifrado = ProcesaDatosCD.ObtienePassword(cadenas[1], false);
+                Console.WriteLine(cifrado);
+                Console.WriteLine();
+            }
+        }
+
+        private static void CifraTextoConDES(string textoOriginal,string password)
+        {
+            Console.WriteLine(string.Format("El texto a cifrar es: {0}", textoOriginal));
+            Console.WriteLine();
+            Console.WriteLine("Texto cifrado:");
+            Console.WriteLine();
+            if (string.IsNullOrEmpty(password))
+                Console.WriteLine(ProcesaDatosCD.CifraTextoEnDES(textoOriginal));
+            else
+                Console.WriteLine(ProcesaDatosCD.CifraTextoEnDES(textoOriginal, password));
+            Console.WriteLine();
+        }
+
+        private static void DescifraTextoConDES(string textoCifrado,string password)
+        {
+            Console.WriteLine(string.Format("El texto cifrado es: {0}", textoCifrado));
+            Console.WriteLine();
+            Console.WriteLine("Texto descifrado:");
+            Console.WriteLine();
+            if (string.IsNullOrEmpty(password))
+                Console.WriteLine(ProcesaDatosCD.DescifraTextoDES(textoCifrado));
+            else
+                Console.WriteLine(ProcesaDatosCD.DescifraTextoDES(textoCifrado, password));
+            Console.WriteLine();
         }
 
         /// <summary>
@@ -55,13 +113,25 @@ namespace MyMUtileriasGenericas
                         case ListaAcciones.AYUDA:
                             CreaInstrucciones();
                             break;
-                        case ListaAcciones.LISTA_ACCIONES:
-                            break;
                         case ListaAcciones.CIFRADO_MD5:
+                            string[] opcionesProcesar = new string[args.Length - 1];
+                            for (int i = 1; i < opcionesProcesar.Length + 1; i++)
+                            {
+                                opcionesProcesar[i - 1] = args[i];
+                            }
+                            ProcesaCadenaParaMD5(opcionesProcesar);
                             break;
                         case ListaAcciones.CIFRADO_DES:
+                            if (args.Length >= 3)
+                                CifraTextoConDES(args[1], args[2]);
+                            else
+                                CifraTextoConDES(args[1], string.Empty);
                             break;
                         case ListaAcciones.DESCIFRADO_DES:
+                            if (args.Length >= 3)
+                                DescifraTextoConDES(args[1], args[2]);
+                            else
+                                DescifraTextoConDES(args[1], string.Empty);
                             break;
                         case ListaAcciones.CIFRADO_RFC289:
                             break;
