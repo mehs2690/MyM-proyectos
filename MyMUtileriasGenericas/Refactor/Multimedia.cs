a1ce4d5a88ef73262aa1d7f4c9d22c3216a1008c
Mauro E. Henaro <muro.mehs.stroke.2672@gmail.com>
2016-01-12 17:28:24 -0600

    Se agregan clases y métodos para solucionar multimedia

---
+++ MyMUtileriasGenericas/Refactor/Multimedia.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMUtileriasGenericas.Refactor
+{
+    /// <summary>
+    /// Clase encargada de procesar peticiones 
+    /// de tipo multimedia, tales como correo electrónico
+    /// compresión de archivos
+    /// </summary>
+    /// <remarks>
+    ///  Desarrollado por: T.P. Mauro Etzael Henaro Sánchez
+    ///  Version: 12.01.2015
+    /// </remarks>
+    public static class Multimedia
+    {
+        public static string smtpAdress = string.Empty;
+        public static int portNumber = 0;
+        public static bool enableSSL = true;
+
+        /// <summary>
+        /// Método que envia correos electrónicos
+        /// </summary>
+        /// <param name="correoDe">correo electrónico remitente</param>
+        /// <param name="correoPara">lista de correos electrónicos receptores</param>
+        /// <param name="password">clave de acceso del correo remitente</param>
+        /// <param name="asunto">describe el asunto del correo</param>
+        /// <param name="cuerpoCorreo">describe el contenido del correo</param>
+        /// <param name="archivosAdjuntos">lista de rutas de archivos adjuntos en el correo</param>
+        public static void EnviaCorreo(string correoDe,List<string> correoPara,string password,string asunto,string cuerpoCorreo,params string[] archivosAdjuntos)
+        {
+            try
+            {
+                using (MailMessage mail= new MailMessage())
+                {
+                    mail.From = new MailAddress(correoDe);
+                    foreach (string correoDestino in correoPara)
+                        mail.To.Add(correoDestino);
+                    mail.Subject = asunto;
+                    mail.Body = cuerpoCorreo;
+                    mail.IsBodyHtml = true;
+                    foreach (string archivo in archivosAdjuntos)
+                        mail.Attachments.Add(new Attachment(archivo));
+                    using (SmtpClient smtp = new SmtpClient(smtpAdress, portNumber))
+                    {
+                        smtp.Credentials = new NetworkCredential(correoDe, password);
+                        smtp.EnableSsl = enableSSL;
+                        smtp.Send(mail);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al enviar el correo electrónico: " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Método que comprime archivos
+        /// </summary>
+        /// <param name="archivos"></param>
+        public static string ComprimeArchivos(params string[] archivos)
+        {
+            string rutaArchivoComprimido = string.Empty;
+
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return rutaArchivoComprimido;
+        }
+    }
+}
