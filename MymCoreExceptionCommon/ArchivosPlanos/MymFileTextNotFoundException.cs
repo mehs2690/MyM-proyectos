16bd7c4eb9734667036996eaf2469f3523c63848
Etzael Henaro Sánchez <muro.mehs.stroke.2672@gmail.com>
2016-08-01 00:07:35 -0500

    se eliminan los proyectos de la fase uno de mym. Se agrega el proyecto de excepciones del common core  Changes to be committed: 	new file:   MymCoreExceptionCommon/ArchivosPlanos/MymFileTextNotFoundException.cs 	new file:   MymCoreExceptionCommon/DataAccess/MymConnectionException.cs 	new file:   MymCoreExceptionCommon/Refactor/MymSendMailException.cs 	new file:   MymCoreExceptionCommon/Seguridad/MymEncryptDataException.cs

---
+++ MymCoreExceptionCommon/ArchivosPlanos/MymFileTextNotFoundException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MymCoreExceptionCommon.ArchivosPlanos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MymFileTextNotFoundException: Exception
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MymFileTextNotFoundException()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public MymFileTextNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public MymFileTextNotFoundException(string message,Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
---
+++ MymCoreExceptionCommon/DataAccess/MymConnectionException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MymCoreExceptionCommon.DataAccess
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MymConnectionException:Exception 
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MymConnectionException()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public MymConnectionException(string message) : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public MymConnectionException(string message,Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
